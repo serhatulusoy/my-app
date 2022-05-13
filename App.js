f3621e358d3cb1f2edabed9ccf38fce61d50d9d4
serhatulusoy <ulusoyserhat@hotmail.com>
2022-05-13 13:42:23 +0300

    env port

--- App.js
+++ App.js
@@ -1,11 +1,11 @@
 const express = require('express')
 const app = express()
-const port = 3000
+var port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
-
+var port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
